2e294a3f61ebc79ca7c435fc589a6dba7970f0c5
those-years <40433078+those-years@users.noreply.github.com>
2018-06-28 05:51:59 +0800

    Add files via upload

--- read.cpp
+++ read.cpp
@@ -131,17 +131,76 @@ void modify()		//修改职工工资数据
 		cin>>modify_pay_wages;
 	cout<<"请输入修改后的绩效工资"<<endl;
 		cin>>modify_merit_pay;
-		 
+	
 }
 
-void del()			//删除职工工资数据 
+
+void del()			//删除职工工资数据 976872969
 {
-	
+	char gonghao[10];
+	int i,j,k,h;
+	cout<<"请输入需要删除的职工工资数据的工号：";
+	cin>>gonghao;
+	for(int i=0;i<n;i++)				//将需要删除的职工工资数据导出来 
+		if(strcmp(GZZG[i].number,gonghao)==0)
+		{
+			cout<<"工号："<<GZZG[i].number<<endl;
+			cout<<"姓名："<<GZZG[i].name<<endl;
+			cout<<"岗位工资："<<GZZG[i].wage<<endl;
+			cout<<"薪级工资："<<GZZG[i].pay_wages<<endl;
+			cout<<"职务津贴"<<GZZG[i].post_allowance<<endl;
+			cout<<"绩效工作："<<GZZG[i].merit_pay<<endl;
+			cout<<"应发工资："<<GZZG[i].should_pay<<endl;
+			cout<<"个人所得税："<<GZZG[i].personal_income_tax<<endl;
+			cout<<"实发工资："<<GZZG[i].real_pay<<endl;
+			cout<<"i="<<i<<endl;
+			h=i;
+			cout<<"h="<<h<<endl;
+			break;
+		} 
+	cout<<"|*********************************|"<<endl;
+	cout<<"|请确认该职工工资数据是否正确!!"<<endl; 
+	cout<<"|请输入1执行删除操作："<<endl;
+	cout<<"|如需放弃请输入2："<<endl;
+	cout<<"|*********************************|"<<endl;
+	cin>>j;
+	if(j==2)
+	{
+		cout<<"退出该操作;请输入xxxx（未实现）" <<endl;
+	}
+	else if(j==1)
+	{
+		for(k==h;k<n-1;k++)
+		{
+			GZZG[k]=GZZG[k+1];
+		}
+		cout<<"修改完成！！"<<endl;
+	}
+	if(h==n)
+	{
+		cout<<"删除的员工为最后一个:"<<endl; 
+	}
+	else
+	{
+	cout<<"工号："<<GZZG[h].number<<endl;
+	cout<<"姓名："<<GZZG[h].name<<endl;
+	cout<<"岗位工资："<<GZZG[h].wage<<endl;
+	cout<<"薪级工资："<<GZZG[h].pay_wages<<endl;
+	cout<<"职务津贴"<<GZZG[h].post_allowance<<endl;
+	cout<<"绩效工作："<<GZZG[h].merit_pay<<endl;
+	cout<<"应发工资："<<GZZG[h].should_pay<<endl;
+	cout<<"个人所得税："<<GZZG[h].personal_income_tax<<endl;
+	cout<<"实发工资："<<GZZG[h].real_pay<<endl;
+	}
 } 
+
+
 
 void add()			//添加职工工资数据 
 {
-		
+	int i;
+	
+	
 }
 
 
@@ -198,12 +257,34 @@ void grsds()		//计算个人所得税
 	}
 }
 
+
+
+void menu()
+{
+	cout<<"***************************|"<<endl;
+	cout<<"欢迎进入菜单！！"<<endl;
+	cout<<"查询职工工资详细信息请输入1"<<endl;
+	cout<<"修改职工工资详细信息请输入2"<<endl;
+	cout<<"添加新的职工工资信息请输入3"<<endl;
+	cout<<"删除职工工资请输入4"<<endl;
+	cout<<"保存当前操作请输入5"<<endl;
+	cout<<"浏览所有职工工资信息请输入6"<<endl;
+	cout<<"退出菜单请输入7"<<endl;	
+}			
+
+
+
 int main()
 {
 	int i; 
 	read(); 
 	cout<<endl; 
-	cout<<"hello熊岚滢！！！"<<endl;
+	cout<<"		"<<"		"<<"hello熊岚滢！！！"<<endl;
+	cout<<endl;
+	cout<<"		"<<"||***************************************||" <<endl;
+	cout<<"		"<<"||开始进入菜单，请在进入菜单后按提示操作 ||"<<endl; 
+	cout<<"		"<<"||***************************************||" <<endl;
+	
 /*	for(i=0;i<n;i++)
 		cout<<GZZG[i].number<<" "<<GZZG[i].name<<" "<<GZZG[i].wage<<" "
 			<<GZZG[i].pay_wages<<" "<<GZZG[i].post_allowance<<" "<<GZZG[i].merit_pay<<endl;
@@ -214,8 +295,9 @@ int main()
 //	write();
 //	find();
 	list();
-	grsds();
-	cout<<GZZG[1].personal_income_tax<<endl;
-	modify();
+//	grsds();
+//	cout<<GZZG[1].personal_income_tax<<endl;
+	//modify();
+	del();
 	return 0;
 } 
